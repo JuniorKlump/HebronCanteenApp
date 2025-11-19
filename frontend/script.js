c090849e6741b9efd62141882cc2874f04ad0d05
JuniorKlump <jr.klump@proton.me>
2025-11-19 16:53:24 +0530

    Fix domain mistake

--- frontend/script.js
+++ frontend/script.js
@@ -8,8 +8,7 @@ const cap = -2000
 if (new Date().getMonth() == 3 && new Date().getDate() == 1) {
    showBillGates = "True"
 } else { showBillGates = "False" }
-const server = "http://presto.local:5500"
-const port = 5500
+const server = "https://hebcanteentabs.onrender.com"
 var pword = ""
 
 try {
