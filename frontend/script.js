983538dde31ac405b110356138256ed33e21d1bf
JuniorKlump <jr.klump@proton.me>
2026-02-07 21:40:10 +0530

    even more debugging

--- frontend/script.js
+++ frontend/script.js
@@ -280,8 +280,8 @@ document.addEventListener('DOMContentLoaded', () => {
 document.getElementById('passwordform').addEventListener('submit',l => {l.preventDefault();login();});
 document.getElementById('tabaddform').addEventListener('submit',l => {l.preventDefault();addtab();});
 document.addEventListener('click', (event) => { 
-   alert(`${event.target.getAttribute('type')} ${event.target.getAttribute('class')}`)
-   if(event.target.getAttribute('type') != 'button' && event.target.getAttribute('class') != 'floating-box') {
+   alert(`${event.target.nodeName} ${event.target.getAttribute('class')}`)
+   if(event.target.nodeName != 'BUTTON' && event.target.getAttribute('class') != 'floating-box') {
       hideall();
    }})
 var autoupdate = setInterval(getTabs, 3000);
