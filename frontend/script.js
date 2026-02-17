a5e134e421485f4c4454e50ceac63a9955536218
JuniorKlump <jr.klump@proton.me>
2026-02-17 05:39:10 +0000

    Oh, it was that simple?

--- frontend/script.js
+++ frontend/script.js
@@ -324,7 +324,7 @@ document.getElementById('passwordform').addEventListener('submit',l => {l.preven
 document.getElementById('tabaddform').addEventListener('submit',l => {l.preventDefault();addtab();});
 document.addEventListener('click', (event) => { 
    //alert(`${event.target.nodeName} ${event.target.getAttribute('class')}`)
-   if((event.target.className == "background-dismiss" || event.target.nodeName != "HTML")){//event.target.nodeName != 'BUTTON' && event.target.nodeName != 'DIV' )) {
+   if((event.target.className == "background-dismiss" || event.target.nodeName == "HTML")&&event.target.nodeName != "BUTTON"){//event.target.nodeName != 'BUTTON' && event.target.nodeName != 'DIV' )) {
       //alert(event.target.className)
     hideall();
    }
