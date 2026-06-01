c8f8eb8cb3d8fe4071b7fc2fbf49a0d690b2366e
JuniorKlump <jr.klump@proton.me>
2026-06-01 14:54:34 +0000

    Update message to make it more universal.

--- TODO.md
+++ TODO.md
@@ -1,5 +1,5 @@
 ## Todo
 - [ ] Add an import/export feature
 - [x] Add letter filters
-- [ ] Reverse buttons to make them more natural.
+- [x] Reverse buttons to make them more natural.
 
--- frontend/script.js
+++ frontend/script.js
@@ -330,4 +330,4 @@ document.addEventListener('click', (event) => {
    }
 })
 var autoupdate = setInterval(getTabs, 3000);
-alert("Notice: \n You're using a free plan for your deployment of hebcanteentabs. \n This will expire, and delete data, on April 22, 2026, unless upgraded.")
+alert("Notice: \nThis website is currently in testing, which may result in unintentional bugs and deletion. \nPlease bear with me for now.")
